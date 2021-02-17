dc21300c15473bbde3474888a79f508661c5f8ce
ryan erswell <ersie93@googlemail.com>
2021-02-17 21:14:10 +0000

    homepage done with no routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+4.times do 
+	Flat.create(
+		name: Faker::TvShows::BigBangTheory.character,
+  		address: Faker::Address.street_address,
+  		description: Faker::Movies::LordOfTheRings.location,
+  		price_per_night: rand(50..150),
+  		number_of_guests: rand(1..5)
+		)
+end
